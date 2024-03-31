12170876f95fd64b6f19979e42e1d9dc7a9ff83b
Joao <joaopriss@gmail.com>
2024-03-31 16:26:41 -0500

    clase de maven, log

---
+++ ejemplMAven/src/main/java/com/krakedev/ejemplMAven/App.java
@@ -0,0 +1,13 @@
+package com.krakedev.ejemplMAven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ejemplMAven/src/main/java/com/krakedev/ejemplMAven/TestAdmin.java
@@ -0,0 +1,8 @@
+package com.krakedev.ejemplMAven;
+
+public class TestAdmin {
+	public static void main(String[] args) {
+		Admin admin = new Admin();
+		admin.agregar();
+	}
+}
