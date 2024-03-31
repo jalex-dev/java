6efb10b2bae00f07ab82a472d2906b0bbae451eb
Joao <joaopriss@gmail.com>
2024-03-30 23:27:19 -0500

    Reto 28 | ejercicios estaticos

---
+++ Estatico/src/com/krakedev/estaticos/ejercicio/utils/DiaSemana.java
@@ -0,0 +1,13 @@
+package com.krakedev.estaticos.ejercicio.utils;
+
+public class DiaSemana {
+	
+	public static final int LUNES=0;
+	public static final int MARTES=1;
+	public static final int MIERCOLES=2;
+	public static final int JUEVES=2;
+	public static final int VIERNES=4;
+	public static final int SABADO= 5;
+	public static final int DOMINGO= 6;
+
+}
